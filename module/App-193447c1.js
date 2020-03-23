ba70fb65cd02a0454c36ebf45f47e7eedaf43a68
Calvin Tam <realcalvintam@gmail.com>
2020-03-23 19:07:39 +0800

    First commit

---
+++ module/App-193447c1.js
@@ -0,0 +1,2 @@
+export{A as default}from"./App-030e40b2.js";
+//# sourceMappingURL=App-193447c1.js.map
---
+++ module/main.js
@@ -0,0 +1,2 @@
+import("./App-193447c1.js").then(({default:t})=>{new t({target:document.body})});
+//# sourceMappingURL=main.js.map
