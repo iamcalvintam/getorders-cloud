4e2eac511fbd815af37bfd27f85b529c0235191f
Calvin Tam <realcalvintam@gmail.com>
2020-05-02 16:03:02 +0800

    no message

--- module/main.js
+++ module/main.js
@@ -1,2 +1,2 @@
-import("./App-64be4c83.js").then(({default:e})=>{new e({target:document.body})});
+import("./App-a2ba15a9.js").then(({default:t})=>{new t({target:document.body})});
 //# sourceMappingURL=main.js.map
