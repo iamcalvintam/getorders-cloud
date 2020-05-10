6d8658700a7ebfef69a1d5ee8dd23209c21d7bc8
Calvin Tam <realcalvintam@gmail.com>
2020-05-11 00:01:50 +0800

    no message

---
+++ module/App-0fa16495.js
@@ -0,0 +1,2 @@
+export{A as default}from"./App-dd60b285.js";
+//# sourceMappingURL=App-0fa16495.js.map
---
+++ module/main.js
@@ -0,0 +1,2 @@
+import("./App-0fa16495.js").then(({default:t})=>{new t({target:document.body})});
+//# sourceMappingURL=main.js.map
