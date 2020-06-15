7c3b7e0922a2f4cdbcade6d45cba45f090ec8112
Calvin Tam <realcalvintam@gmail.com>
2020-06-15 23:33:50 +0800

    no message

---
+++ module/App-074f18fa.js
@@ -0,0 +1,2 @@
+export{A as default}from"./App-f22176fd.js";
+//# sourceMappingURL=App-074f18fa.js.map
--- module/main.js
+++ module/main.js
@@ -1,2 +1,2 @@
-import("./App-8ef13d5d.js").then(({default:e})=>{new e({target:document.body})});
+import("./App-074f18fa.js").then(({default:t})=>{new t({target:document.body})});
 //# sourceMappingURL=main.js.map
