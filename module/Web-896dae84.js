8595f8d13bdf01ea6b14cb37ba0b59a1ef58a117
Calvin Tam <realcalvintam@gmail.com>
2021-02-22 14:35:00 +0800

    no message

---
+++ module/Web-896dae84.js
@@ -0,0 +1,2 @@
+export{W as default}from"./Web-e295f046.js";
+//# sourceMappingURL=Web-896dae84.js.map
--- module/main.js
+++ module/main.js
@@ -1,2 +1,2 @@
-import("./Web-1da0434c.js").then(({default:e})=>{new e({target:document.body})});
+import("./Web-896dae84.js").then(({default:e})=>{new e({target:document.body})});
 //# sourceMappingURL=main.js.map
